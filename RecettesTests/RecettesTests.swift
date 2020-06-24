6d0116ba699c60a1378c5d638a7a76003aee0aa7
kenGassy <ken.gassy@gmail.com>
2020-06-24 11:38:11 +0200

    Initial Commit

---
+++ RecettesTests/RecettesTests.swift
@@ -0,0 +1,34 @@
+//
+//  RecettesTests.swift
+//  RecettesTests
+//
+//  Created by Ken Gassy on 24/06/2020.
+//  Copyright © 2020 Ken Gassy. All rights reserved.
+//
+
+import XCTest
+@testable import Recettes
+
+class RecettesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
